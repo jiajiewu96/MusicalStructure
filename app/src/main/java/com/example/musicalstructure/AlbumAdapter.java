7b4f47db2c64ddc90f23f4e8662bd4e355f1890e
wackieju <jiajie.wu96@gmail.com>
2019-07-17 16:51:29 -0700

    added logic for AlbumAdapter

--- app/src/main/java/com/example/musicalstructure/AlbumAdapter.java
+++ app/src/main/java/com/example/musicalstructure/AlbumAdapter.java
@@ -1,9 +1,11 @@
 package com.example.musicalstructure;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,6 +15,18 @@ public class AlbumAdapter extends ArrayAdapter<Song> {
     }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.album_list_item, parent, false);
+        }
+        Song song = getItem(position);
+
+        TextView albumTextView = (TextView) listItemView.findViewById(R.id.album_list_album_name);
+        TextView artistTextView = (TextView) listItemView.findViewById(R.id.album_list_artist_name);
+
+        albumTextView.setText(song.getmAlbumName());
+        artistTextView.setText(song.getArtistName());
+
         return super.getView(position, convertView, parent);
     }
 }
