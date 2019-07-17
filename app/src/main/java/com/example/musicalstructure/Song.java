5ea3ae53a7295d177373e60b91ee46f58de2cc3a
wackieju <jiajie.wu96@gmail.com>
2019-07-17 15:39:10 -0700

    Set field for song class

--- app/src/main/java/com/example/musicalstructure/Song.java
+++ app/src/main/java/com/example/musicalstructure/Song.java
@@ -1,4 +1,23 @@
 package com.example.musicalstructure;
 
 public class Song {
+    private String mSongName;
+    private String mAlbumName;
+    private String mArtistName;
+
+    public Song (String songName, String albumName, String artistName){
+        mSongName = songName;
+        mAlbumName = albumName;
+        mArtistName = artistName;
+    }
+
+    public String getSongName() {
+        return mSongName;
+    }
+    public String getmAlbumName() {
+        return mAlbumName;
+    }
+    public String getArtistName(){
+        return mArtistName;
+    }
 }
