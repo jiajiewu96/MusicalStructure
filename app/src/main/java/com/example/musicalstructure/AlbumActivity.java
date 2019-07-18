67dab040a0f2b2a6619fbdfc0f315c4c03c909dc
wackieju <jiajie.wu96@gmail.com>
2019-07-17 17:15:09 -0700

    Updated logic in adapter

--- app/src/main/java/com/example/musicalstructure/AlbumActivity.java
+++ app/src/main/java/com/example/musicalstructure/AlbumActivity.java
@@ -3,6 +3,7 @@ package com.example.musicalstructure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -15,9 +16,11 @@ public class AlbumActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
 
-
         ArrayList<Song> songs = songDataManager.getSongs();
 
+        AlbumAdapter albumAdapter = new AlbumAdapter(this, songs);
 
+        ListView listView = findViewById(R.id.album_list);
+        listView.setAdapter(albumAdapter);
     }
 }
--- app/src/main/java/com/example/musicalstructure/AlbumAdapter.java
+++ app/src/main/java/com/example/musicalstructure/AlbumAdapter.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,9 +22,17 @@ public class AlbumAdapter extends ArrayAdapter<Song> {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.album_list_item, parent, false);
         }
         Song song = getItem(position);
+        final String albumName = song.getmAlbumName();
 
+        listItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), albumName, Toast.LENGTH_SHORT).show();
+            }
+        });
         TextView albumTextView = (TextView) listItemView.findViewById(R.id.album_list_album_name);
         TextView artistTextView = (TextView) listItemView.findViewById(R.id.album_list_artist_name);
+
 
         albumTextView.setText(song.getmAlbumName());
         artistTextView.setText(song.getArtistName());
